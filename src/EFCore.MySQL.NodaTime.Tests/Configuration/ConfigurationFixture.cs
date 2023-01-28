53448fb4481e7e7e3efacaa6689ac6b0ff7dd528
daylanvig <daylanvig@gmail.com>
2023-01-28 11:39:11 -0700

    [Feat] Add test configuration file/provider

---
+++ src/EFCore.MySQL.NodaTime.Tests/Configuration/ConfigurationFixture.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace EntityFrameworkCore.MySQL.NodaTime.Tests.Configuration;
+
+public class ConfigurationFixture
+{
+    private const string CONNECTION_STRING_KEY = "Default";
+
+    private readonly IConfiguration _configuration;
+
+    public ConfigurationFixture()
+    {
+        _configuration = CreateConfiguration();
+    }
+
+    private static IConfiguration CreateConfiguration()
+    {
+        return new ConfigurationBuilder()
+                        .AddJsonFile("Configuration/appsettings.json")
+                        .Build();
+    }
+
+
+    public string ConnectionString => _configuration.GetConnectionString(CONNECTION_STRING_KEY) ?? throw new Exception("Connection string not found in configuration");
+}
---
+++ src/EFCore.MySQL.NodaTime.Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using NodaTime;
+global using Microsoft.EntityFrameworkCore;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Xunit;
